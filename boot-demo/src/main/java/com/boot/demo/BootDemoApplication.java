9c381e36c91d10997d29702ed78a2e23e8ba94e5
TaeGyeomHwang <jihuhw99@gmail.com>
2024-04-22 17:42:31 +0900

    20240422

--- boot-demo/src/main/java/com/boot/demo/BootDemoApplication.java
+++ boot-demo/src/main/java/com/boot/demo/BootDemoApplication.java
@@ -10,8 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class BootDemoApplication {
 
-    @Value("${frontDomain}")
-    private String frontDomain;
+//    @Value("${frontDomain}")
+//    private String frontDomain;
 
     public static void main(String[] args) {
         SpringApplication.run(BootDemoApplication.class, args);
